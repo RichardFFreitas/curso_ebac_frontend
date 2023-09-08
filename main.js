98d7777f26293e39e0b47a61b94c23f4235b08a1
Richard Freitas <123339564+RichardFFreitas@users.noreply.github.com>
2023-09-07 21:54:04 -0300

    adicionando exercicio ajax

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/RichardFFreitas')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        nameElement.innerText = json.name;
+        usernameElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        reposElement.innerText = json.public_repos;
+        followersElement.innerText = json.followers;
+        followingElement.innerText = json.following;
+        linkElement.href = json.html_url
+    })
+})
